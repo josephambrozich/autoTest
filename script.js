d168ecca1b679e6e4c8123ac1a7f81e0c6502eed
Joseph <josephambrozich@gmail.com>
2019-05-23 15:35:25 -0400

    added ball that moves on click

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(function(){
     $("#counter").text("Counter updated by script file");
+
+    var bLeft = 0;
+    $(document).on('click', function(){
+        bLeft += 5;
+        $(".ball").css('left', bLeft);
+    });
 });
